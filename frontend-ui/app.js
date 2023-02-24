3a5d9a12fad04aa0905d9bdeba1c71a0127a5116
Joffrey Mayer <73405747+joffreymayer@users.noreply.github.com>
2023-02-24 11:38:12 +0100

    Changes to relative path to make ui-demo work

--- frontend-ui/app.js
+++ frontend-ui/app.js
@@ -15,7 +15,7 @@ jobs.forEach(job => {
 
 // This code will randomly allocate images to all the cards (by looping through all the images and allocating each `<img>` an image in my folder):
 let images = document.querySelectorAll('img');
-let imageList = ['/frontend-ui/img/img-1.jpg', '/frontend-ui/img/img-2.jpg', '/frontend-ui/img/img-3.jpg', '/frontend-ui/img/img-4.jpg', '/frontend-ui/img/img-5.jpg', '/frontend-ui/img/img-6.jpg', '/frontend-ui/img/img-7.jpg', '/frontend-ui/img/img-8.jpg']; // List of all(!) the images' paths.
+let imageList = ['/create-ui-based-on-a-database/frontend-ui/img/img-1.jpg', '/create-ui-based-on-a-database/frontend-ui/img/img-2.jpg', '/create-ui-based-on-a-database/frontend-ui/img/img-3.jpg', '/create-ui-based-on-a-database/frontend-ui/img/img-4.jpg', '/create-ui-based-on-a-database/frontend-ui/img/img-5.jpg', '/frontend-ui/img/img-6.jpg', '/create-ui-based-on-a-database/frontend-ui/img/img-7.jpg', '/create-ui-based-on-a-database/frontend-ui/img/img-8.jpg']; // List of all(!) the images' paths.
 
 images.forEach(image => {
     let randomImage = imageList[Math.floor(Math.random()*imageList.length)];
